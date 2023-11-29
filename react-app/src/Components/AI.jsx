1aac1e0f2199ff8547fc37b5f4acd0a52cd9484a
zoya rasheed <zoyaa4840@gmail.com>
2023-11-30 03:30:01 +0500

    DialectAPI

--- react-app/src/Components/AI.jsx
+++ react-app/src/Components/AI.jsx
@@ -23,6 +23,25 @@ function AI() {
         return ({ value: language._id, label: language.name })
 
     })
+
+    const [dialects , setDialects] = useState([]);
+    useEffect(() => {
+        const getDialects = () => {
+            try {
+                const res = axios.get('https://100085.pythonanywhere.com/api/v1/dowell-video-generator/process_text_to_video/?type=get_dialect_list')
+                    .then((res) => setDialects(res.data.response)
+                    )
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        getDialects();
+    }, [])
+
+    const dialectOptions = dialects.map((dialect) => {
+        return { value: dialect._id, label: dialect.name };
+    });
     return (
         <>
             <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '50%' }}>
@@ -30,7 +49,10 @@ function AI() {
                 <div style={{ borderBottom: '1px solid gray', width: '100%' }}></div>
                 <h1 style={{ textAlign: 'center', color: 'green' }}>Welcome to Fliki AI</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ratione rerum officiis velit, facere obcaecati.</p>
-                <Select options={options} value={selectedLang} onChange={(selectedOption) => setSelectedLang(selectedOption)} placeholder="Select a Language" />
+                <div style={{display:'flex',justifyContent:'space-around',width:'100%',marginTop:'10px'}}>
+                <Select options={options} value={selectedLang} onChange={(selectedOption) => setSelectedLang(selectedOption)} styles={{width:'100%'}} placeholder="Select a Language" />
+                <Select options={dialectOptions} styles={{width:'100%'}} placeholder="Select a Dialect" />
+                </div>
             </div>
         </>
     )
